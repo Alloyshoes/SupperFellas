e57a66cc3e8a011f31fec47b4041983519f44392
zenercurrent <11849012+zenercurrent@users.noreply.github.com>
2025-06-01 18:32:40 +0800

    basic auth code using firebase auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import LoginSignup from './pages/LoginSignup/LoginSignup';
 
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// saved in .env file
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 function App() {
 	return (
 		<div className="App">
-			<LoginSignup />
+			<LoginSignup auth={auth}/>
 		</div>
 	);
 }
--- src/pages/LoginSignup/LoginSignup.js
+++ src/pages/LoginSignup/LoginSignup.js
@@ -1,41 +1,78 @@
-import React, { useState } from 'react'
-import './LoginSignup.css'
+import { useState } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import './LoginSignup.css';
 
-import user_icon from '../../assets/person.png'
-import email_icon from '../../assets/email.png'
-import password_icon from '../../assets/password.png'
+import user_icon from '../../assets/person.png';
+import email_icon from '../../assets/email.png';
+import password_icon from '../../assets/password.png';
 
-const LoginSignup = () => {
+const LoginSignup = props => {
+	const auth = props.auth;
 
-  const [action,setAction] = useState("Login");
+	const [action, setAction] = useState("Login");
 
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="text">{action}</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs"></div>
-      {action==="Login" ?<div></div>:<div className="input">
-        <img src={user_icon} alt="" />
-        <input type="text" placeholder="Name"/>
-      </div>}
-      
-      <div className="input">
-        <img src={email_icon} alt="" />
-        <input type="email" placeholder='Email Id'/>
-      </div>
-      <div className="input">
-        <img src={password_icon} alt="" />
-        <input type="password" placeholder='Password'/>
-      </div>
-      {action==="Sign Up"?<div></div>: <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
-      <div className="submit-container">
-        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-      </div>
-    </div>
-  )
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	// temp function just to get the auth working ya
+	async function submit(e) {
+		e.preventDefault();
+
+		if (action === "Login") {
+			await signInWithEmailAndPassword(auth, email, password)
+				.then(creds => {
+					const user = creds.user;
+					console.log("Logged in!");
+					console.log(user);
+				})
+				.catch(error => {
+					console.error(error.code, error.message);
+				});
+
+		} else if (action === "Sign Up") {
+			await createUserWithEmailAndPassword(auth, email, password)
+				.then(creds => {
+					const user = creds.user;
+					console.log("Successfully signed up!");
+					console.log(user);
+				})
+				.catch(error => {
+					console.error(error.code, error.message);
+				});
+
+		} else {
+			console.error("Invalid action state!");
+		}
+	}
+
+	return (
+		<div className="container">
+			<div className="header">
+				<div className="text">{action}</div>
+				<div className="underline"></div>
+			</div>
+			<div className="inputs"></div>
+			{action === "Login" ? <div></div> : <div className="input">
+				<img src={user_icon} alt="" />
+				<input type="text" onChange={(e) => setName(e.target.value)} placeholder="Name" />
+			</div>}
+
+			<div className="input">
+				<img src={email_icon} alt="" />
+				<input type="email" onChange={(e) => setEmail(e.target.value)} required placeholder='Email Id' />
+			</div>
+			<div className="input">
+				<img src={password_icon} alt="" />
+				<input type="password" onChange={(e) => setPassword(e.target.value)} required placeholder='Password' />
+			</div>
+			{action === "Sign Up" ? <div></div> : <div className="forgot-password">Lost Password? <span>Click Here!</span></div>}
+			<div className="submit-container">
+				<div className={action === "Login" ? "submit gray" : "submit"} onClick={(e) => { action === "Sign Up" ? submit(e) : setAction("Sign Up"); }}>Sign Up</div>
+				<div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={(e) => { action === "Login" ? submit(e) : setAction("Login"); }}>Login</div>
+			</div>
+		</div>
+	)
 }
 
-export default LoginSignup
+export default LoginSignup;
